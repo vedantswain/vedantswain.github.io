397bf0b2f5b91a48fc660ca0191381da3065777f
vedantswain <vedant12118@iiitd.ac.in>
2022-07-05 22:20:43 -0400

    completed our story, added randomized names, and added footer

--- assets/wedding_assets/js/mystyle.js
+++ assets/wedding_assets/js/mystyle.js
@@ -11,3 +11,34 @@ function hideMenu() {
   var x = document.getElementById("myTopnav");
   x.className = "topnav";
 }
+
+var couple_names = [
+  ["Sugar", "Spice"],
+  ["Peanut Butter", "Banana"],
+  ["Converse",  "High Socks"],
+  ["Marshmallow",   "Graham Cracker"],
+  ["Spongebob", "Patrick"],
+  ["Pink",  "Green"],
+  ["Bread", "Butter"],
+  ["Banh Mi", "Ca Phe"],
+  ["Guava", "Salt"],
+  ["Bagel", "Cream Cheese"], 
+  ["Westley", "Buttercup"],
+  ["Steve Rogers", "Bucky Barnes"],
+  ["Wanda", "Vision"],
+  ["Han Solo", "Chewbacca"],
+  ["Yuji", "Todo"],
+  ["Chicken", "Waffle"],
+  ["Chai", "Samosa"],
+]
+
+function randomizeName() {
+  random_ix = Math.floor(Math.random() * couple_names.length);
+  couple = couple_names[random_ix]
+  v_tag = document.getElementById("name-v")
+  v_tag.innerHTML = couple[1]
+  k_tag = document.getElementById("name-k")
+  k_tag.innerHTML = couple[0]
+}
+
+randomizeName()
